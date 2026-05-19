48064db2df04b8be8eb6ea570295657ce46d8734
Motkyz <kuznetsov.matvey.2005@mail.ru>
2026-05-19 17:03:19 +0500

    .

--- app/src/main/java/com/example/financier/di/binds/NetworkModule.kt
+++ app/src/main/java/com/example/financier/di/binds/NetworkModule.kt
@@ -13,26 +13,25 @@ import javax.inject.Singleton
 @Module
 object NetworkModule {
 
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(
-
-    ): OkHttpClient {
-        return OkHttpClient.Builder()
-            .connectTimeout(60, TimeUnit.SECONDS)      // Время на установку соединения
-            .readTimeout(120, TimeUnit.SECONDS)        // Время на чтение данных
-            .writeTimeout(120, TimeUnit.SECONDS)       // Время на запись (важно для загрузки файлов)
-            .callTimeout(180, TimeUnit.SECONDS)        // Общий таймаут на весь запрос
-            .retryOnConnectionFailure(true)            // Повторять при ошибках соединения
-            .build()
-    }
+//    @Provides
+//    @Singleton
+//    fun provideOkHttpClient(
+//
+//    ): OkHttpClient {
+//        return OkHttpClient.Builder()
+//            .connectTimeout(60, TimeUnit.SECONDS)      // Время на установку соединения
+//            .readTimeout(120, TimeUnit.SECONDS)        // Время на чтение данных
+//            .writeTimeout(120, TimeUnit.SECONDS)       // Время на запись (важно для загрузки файлов)
+//            .callTimeout(180, TimeUnit.SECONDS)        // Общий таймаут на весь запрос
+//            .retryOnConnectionFailure(true)            // Повторять при ошибках соединения
+//            .build()
+//    }
 
     @Provides
     fun provideNetworkService(
-        okHttpClient: OkHttpClient
+//        okHttpClient: OkHttpClient
     ): NetworkService = Retrofit.Builder()
-        .baseUrl("http://10.0.2.2:8000/api/")
-        .client(okHttpClient)
+        .baseUrl("http://192.168.43.18:8000/api/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create()
